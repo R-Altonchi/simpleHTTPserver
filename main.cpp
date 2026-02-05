eff80df7b3209988284e60f7a7471a6f5218ec85
R-Altonchi <rafik.altonchi.professional@gmail.com>
2026-02-05 14:02:41 +0100

    Unfinished Server Code
    This code is unfinished but will be commited since it will be on github
    now

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <openssl/ssl.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (int argc, char *argv[]) {
+  
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in addr = {
+    AF_INET,
+    htons(8080),
+    0
+  };
+  
+  bind(sockfd, (sockaddr*)&addr, sizeof(addr);
+  listen(sockfd, 10);
+  printf("Server listening on port 8080...\n");
+
+  while (true) {
+  int clientfd = accept(sockfd, NULL, NULL);
+
+  char buffer[1024] = {0};
+  recv(clientfd, buffer, sizeof(buffer) - 1, 0);
+  printf("Request: %s\n", buffer);
+
+  char* file_request = buffer + 5;
+  char response[2048] = {0}; 
+  const char* metadata = "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n";
+  strcpy(response, metadata);
+  
+  //This is to test sending index,html
+  if (strncmp(file_request, " ", 10) == 0) {
+    FILE* f = fopen("index.html", "r");
+    if (f) {
+      fread(response + strlen(metadata), 1, 1024, f);
+      fclose(f);
+    }
+  }else {
+    const char* error = "page not found!";
+    strcat(response, error);
+  }
+  
+  send(clientfd, response, strlen(response), 0);
+  
+  close(clientfd);
+
+  }
+
+  close(sockfd);
+  
+  return 0;
+}
